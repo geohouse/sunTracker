10dd9904d9f67c997264b601ce59c754b547f6f8
geohouse <geo.house7@gmail.com>
2020-10-08 20:50:46 -0600

    sunTracker changes to Julian day calc. Working correctly.

--- sunTracker.js
+++ sunTracker.js
@@ -24,4 +24,20 @@ for(let stepNum = 1; stepNum <= numSteps; stepNum ++){
     dmy.setHours(numHours);
     dmy.setMinutes(numMins);
     console.log(dmy);
+    let dmy_time = dmy.getTime();
+    // Calculation method from here:
+    // https://stackoverflow.com/questions/11759992/calculating-jdayjulian-day-in-javascript
+    // 86400000 is the number of milliseconds in a day to convert the time into the number of days
+    // No longer used - 1440 is the number of minutes in a day, needed because the timezone offset returns in minutes
+    // 2440587.5 is the number of fractional days from the start of the Julian counting to Jan 1st, 1970 (the start of the JavaScript counting)
+    let julianDay = (dmy_time / 86400000) + 2440587.5;
+    //let julianDay = (dmy_time / 86400000) - (dmy.getTimezoneOffset()/1440) + 2440587.5;
+    
+    // 2451545 is the number of days from the start of the Julian counting to y2k
+    let julianCentury = (julianDay - 2451545)/36525
+    console.log(julianDay);
+    console.log(julianCentury);
+
+
+
 }
